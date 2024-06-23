8bcf8c06adb9b34afc005184621b1967fb65e3ae
Elleined <demadegu@gmail.com>
2024-06-23 21:01:37 +0800

    Added circuit breaker and retry application properties

--- resilience4j-caller-service/src/main/java/com/elleined/resilience4j_caller_service/client/FeignCallingClient.java
+++ resilience4j-caller-service/src/main/java/com/elleined/resilience4j_caller_service/client/FeignCallingClient.java
@@ -1,11 +1,27 @@
 package com.elleined.resilience4j_caller_service.client;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "${calling-service.name}", url = "${calling-service.base-url}")
+@FeignClient(
+        name = "${calling-service.name}", // Just like when you are using the @Value to get value from application.properties
+        url = "${calling-service.base-url}" // Just like when you are using the @Value to get value from application.properties
+)
 public interface FeignCallingClient {
 
     @GetMapping
+    @CircuitBreaker(name = "callingServiceCircuitBreaker", fallbackMethod = "simpleEndpointFallbackMethod")
     String simpleEndpoint();
+
+    /**
+     * You but you can return anything that you want. For simplicity, we will return String for now.
+     * And execute code here for properly handling the fallback method.
+     *
+     * @param throwable is used for the circuit breaker to call the fallback method
+     * @return String
+     */
+    default String simpleEndpointFallbackMethod(Throwable throwable) {
+        return "Circuit breaker fallback method called!";
+    }
 }
