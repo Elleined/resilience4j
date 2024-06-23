05355373793e62bbc9623d62df53e0928908963d
Elleined <demadegu@gmail.com>
2024-06-23 22:40:05 +0800

    Added circuit breaker and retry application properties

--- resilience4j-caller-service/src/main/java/com/elleined/resilience4j_caller_service/CallerController.java
+++ resilience4j-caller-service/src/main/java/com/elleined/resilience4j_caller_service/CallerController.java
@@ -1,6 +1,6 @@
 package com.elleined.resilience4j_caller_service;
 
-import com.elleined.resilience4j_caller_service.client.FeignCallingClient;
+import com.elleined.resilience4j_caller_service.client.CallingFeignClient;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,11 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class CallerController {
 
-    private final FeignCallingClient feignCallingClient;
+    private final CallingFeignClient callingFeignClient;
 
     @GetMapping
     public String callingMethod() {
-        String result = feignCallingClient.simpleEndpoint();
+        String result = callingFeignClient.simpleEndpoint();
         return "Caller service successfully called the method in calling service which has result of: " + result;
     }
 }
--- resilience4j-caller-service/src/main/java/com/elleined/resilience4j_caller_service/client/CallingFeignClient.java
+++ resilience4j-caller-service/src/main/java/com/elleined/resilience4j_caller_service/client/CallingFeignClient.java
@@ -5,23 +5,23 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @FeignClient(
-        name = "${calling-service.name}", // Just like when you are using the @Value to get value from application.properties
+        value = "${calling-service.name}", // Just like when you are using the @Value to get value from application.properties
         url = "${calling-service.base-url}" // Just like when you are using the @Value to get value from application.properties
 )
-public interface FeignCallingClient {
+public interface CallingFeignClient {
 
     @GetMapping
-    @CircuitBreaker(name = "callingServiceCircuitBreaker", fallbackMethod = "simpleEndpointFallbackMethod")
+    @CircuitBreaker(name = "simpleEndpointCircuitBreaker", fallbackMethod = "simpleEndpointFallbackMethod")
     String simpleEndpoint();
 
     /**
      * You but you can return anything that you want. For simplicity, we will return String for now.
      * And execute code here for properly handling the fallback method.
      *
-     * @param throwable is used for the circuit breaker to call the fallback method
+     * @param exception is used for the circuit breaker to call the fallback method
      * @return String
      */
-    default String simpleEndpointFallbackMethod(Throwable throwable) {
+    default String simpleEndpointFallbackMethod(Exception exception) {
         return "Circuit breaker fallback method called!";
     }
 }
