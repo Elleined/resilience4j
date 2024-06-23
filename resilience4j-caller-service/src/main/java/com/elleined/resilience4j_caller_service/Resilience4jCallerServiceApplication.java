8842a0914715e6b72e09bbd0df354c396ab4e85b
Elleined <demadegu@gmail.com>
2024-06-23 22:46:41 +0800

    Added circuit breaker and retry application properties

--- resilience4j-caller-service/src/main/java/com/elleined/resilience4j_caller_service/Resilience4jCallerServiceApplication.java
+++ resilience4j-caller-service/src/main/java/com/elleined/resilience4j_caller_service/Resilience4jCallerServiceApplication.java
@@ -1,11 +1,8 @@
 package com.elleined.resilience4j_caller_service;
 
-import com.elleined.resilience4j_caller_service.client.MessageErrorDecoder;
-import feign.codec.ErrorDecoder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -15,8 +12,4 @@ public class Resilience4jCallerServiceApplication {
 		SpringApplication.run(Resilience4jCallerServiceApplication.class, args);
 	}
 
-	@Bean
-	public ErrorDecoder errorDecoder() {
-		return new MessageErrorDecoder();
-	}
 }
